1aaa08499a5a1ffb2cbfb8ddc714ec694a458787
Erich Hafenmaier <ehafenmaier@gmail.com>
2022-01-16 10:45:17 -0700

    Add basic hello world program

---
+++ hello-world/main.go
@@ -0,0 +1,10 @@
+// The 'main' package declaration is only used for building an executable file
+// and must also contain a 'main()' function. Any other package declaration will
+// build a reusable library file.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
