b4962efdf56cc98c614247c231c13f539590101b
Erich Hafenmaier <ehafenmaier@gmail.com>
2022-01-22 16:36:39 -0700

    Add call to deal function and print out hand plus remaining cards

--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	cards.shuffle()
-	cards.print()
+	
+	hand, remainingCards := deal(cards, 5)
+
+	fmt.Println("\nHand\n----")
+	hand.print()
+	fmt.Println("\nRemaining Cards\n---------------")
+	remainingCards.print()
 }
