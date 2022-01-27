8d947829f3e7da14f3069c39347ce066825a22b8
Erich Hafenmaier <ehafenmaier@gmail.com>
2022-01-26 21:13:37 -0700

    Different ways to declare a map as well as adding/deleting keys

--- maps/main.go
+++ maps/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
-	}
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// 	"blue":  "#0000ff",
+	// }
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
