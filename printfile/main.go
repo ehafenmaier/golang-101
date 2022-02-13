bf11cdd62d57988d2d2ae5d92070905b49a28c3b
Erich Hafenmaier <ehafenmaier@gmail.com>
2022-02-13 12:30:13 -0700

    Rework print file project to use interfaces that the file type adheres to

--- printfile/main.go
+++ printfile/main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
+
+type fileWriter struct {}
 
 func main() {
 	// Store the name of the file passed in to a separate variable
 	fileName := os.Args[1]
 
-	// Use the ReadFile function to get the contents of
-	// the file as a byte slice
-	contents, err := os.ReadFile(fileName)
+	// Open the file as read only
+	file, err := os.Open(fileName)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
-	// Print out the byte slice contents as a string
-	fmt.Println(string(contents))
+	// Create a fileWriter
+	fw := fileWriter{}
+
+	// Use io.Copy to copy file contents to new fileWriter
+	io.Copy(fw, file)
+}
+
+// Function with fileWriter receiver that adheres to io.Writer interface
+func (fileWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("The file contents is this many bytes:", len(bs))
+
+	return len(bs), nil
 }
