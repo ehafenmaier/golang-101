3e7c34f2dd5321e88d5edbc67485613b901fa39c
Erich Hafenmaier <ehafenmaier@gmail.com>
2022-02-19 09:04:30 -0700

    Add channel communication to websitte status check program

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// Make a channel that accepts type string
+	c := make(chan string)
+
+	// Loop over the links to check
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<- c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep, it's up"
 }
